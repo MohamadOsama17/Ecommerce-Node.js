476ccab61f50d38da49241f4268f4cf66875c2e2
MohamadOsama17 <mohamad.abu.ramadan@gmail.com>
2023-09-17 00:52:50 +0300

    - delete & update product conroller logic

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 const { isValidObjectId } = require('mongoose');
-const Product = require('../models/productsModel')
+const Product = require('../models/productsModel');
+const pagenationResponse = require('../response/pagenationResponse')
 
 
 const getProductById = async (req, res, next) => {
@@ -44,15 +45,83 @@ const createProduct = async (req, res, next) => {
   }
 }
 
-const deleteProduct = (req, res, next) => {
-  const productId = req.params.productId;
-  res.status(200).send(`delete details for product : ${productId}`);
+const deleteProduct = async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
+    if (!isValidObjectId(productId)) {
+      return res.status(404).json({
+        success: false,
+        message: 'No product found !'
+      });
+    }
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: 'No product found !'
+      });
+    }
+    await product.deleteOne();
+    return res.status(200).json({
+      success: true,
+      message: "Product deleted successfully !"
+    });
+  } catch (error) {
+    return next(error)
+  }
 };
 
-const updateProduct = (req, res, next) => {
-  const productId = req.params.productId;
-  const { body } = req;
-  res.status(200).send(`update details for product : ${productId},body :${body.name}`)
+const updateProduct = async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
+    if (!isValidObjectId(productId)) {
+      return res.status(404).json({
+        success: false,
+        message: 'No product found !'
+      });
+    }
+
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: 'No product found !'
+      });
+    }
+
+    const { name, price, coverImage } = req.body;
+    if (name) {
+      product.name = name;
+    }
+    if (price) {
+      product.price = price;
+    }
+    if (coverImage) {
+      product.coverImage = coverImage;
+    }
+    const result = await product.save();
+    return res.status(200).json({ success: true, product: result })
+  } catch (error) {
+    next(error);
+  }
 };
 
-module.exports = { getProductById, deleteProduct, updateProduct, createProduct }
+const getAllProdcts = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const totalDocs = await Product.countDocuments();
+    const pagination = pagenationResponse({ page, totalDocs, limit });
+    const products = await Product.find({}).limit(limit).skip((page - 1) * limit);
+    return res.status(200).json({
+      success: true,
+      products,
+      pagination,
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports = { getProductById, deleteProduct, updateProduct, createProduct, getAllProdcts }
