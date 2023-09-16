1048d625a38afe1d34e748d97535ed5fb4c3ae39
MohamadOsama17 <mohamad.abu.ramadan@gmail.com>
2023-09-16 04:24:46 +0300

    - add logic for fetch & create product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,8 +1,48 @@
+const { isValidObjectId } = require('mongoose');
+const Product = require('../models/productsModel')
 
-const getProductById = (req, res, next) => {
-  const productId = req.params.productId;
-  res.status(200).send(`get details for product : ${productId}`);
+
+const getProductById = async (req, res, next) => {
+  try {
+
+    const productId = req.params.productId;
+    if (!productId) {
+      return res.status(400).json({
+        success: false,
+        message: 'You should pass product id'
+      });
+    }
+    if (!isValidObjectId(productId)) {
+      return res.status(404).json({
+        success: false,
+        message: 'No product found related !qw'
+      });
+    }
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: 'No product found related wq!'
+      });
+    }
+    return res.status(200).json({ product });
+  } catch (error) {
+    next(error)
+  }
 };
+
+const createProduct = async (req, res, next) => {
+  try {
+    const { name, price, quantity, coverImage, images } = req.body;
+    const product = await Product.create({ name, price, quantity, coverImage, images });
+    res.status(201).json({
+      'success': true,
+      product,
+    })
+  } catch (error) {
+    return next(error)
+  }
+}
 
 const deleteProduct = (req, res, next) => {
   const productId = req.params.productId;
@@ -15,4 +55,4 @@ const updateProduct = (req, res, next) => {
   res.status(200).send(`update details for product : ${productId},body :${body.name}`)
 };
 
-module.exports = { getProductById, deleteProduct, updateProduct }
+module.exports = { getProductById, deleteProduct, updateProduct, createProduct }
