f2f16a2c7b116d2c2abc9c77e1a179e3628c23e7
MohamadOsama17 <mohamad.abu.ramadan@gmail.com>
2023-09-16 04:23:55 +0300

    - add role access

--- routes/product/product.js
+++ routes/product/product.js
@@ -1,18 +1,18 @@
 const router = require('express').Router();
+const appRoles = require('../../config/roles');
 const productController = require('../../controllers/productController');
+const verifyRole = require('../../middlewares/authroizeRole');
 
 
 router.route('/').get((req, res, next) => {
   res.status(200).send('<h1>page route</h1>')
-}).post((req, res, next) => {
-  res.status(201).send('product created...')
-});
+}).post(verifyRole([appRoles.Admin, appRoles.Editor]), productController.createProduct);
 
 
 router.route('/:productId')
   .get(productController.getProductById)
-  .delete(productController.deleteProduct)
-  .patch(productController.updateProduct);
+  .delete(verifyRole([appRoles.Admin]), productController.deleteProduct)
+  .patch(verifyRole([appRoles.Admin]), productController.updateProduct);
 
 
 module.exports = router;
