61d25c147afcde425a9e4d085005ca147775d33d
MohamadOsama17 <mohamad.abu.ramadan@gmail.com>
2023-09-24 19:33:32 +0300

    - group in auth controller

--- routes/auth/refresh_token/refresh_token.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const refreshTokenController = require('../../../controllers/refreshTokenController');
-
-
- router.post('/',refreshTokenController.refreshToken);
-
-
-module.exports = router;
--- routes/auth/register/register.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const registerController = require('../../../controllers/registerController')
-
-router.post('/', registerController.registerUser);
-
-module.exports = router;
