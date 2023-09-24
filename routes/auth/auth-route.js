0fd585671f3272b5ec733f4f4a6f3439cfd35f97
MohamadOsama17 <mohamad.abu.ramadan@gmail.com>
2023-09-24 19:35:36 +0300

    - group auth in one route

--- routes/auth/auth-route.js
+++ routes/auth/auth-route.js
@@ -2,6 +2,12 @@
 const router = require('express').Router();
 const authController = require('../../controllers/authController');
 
+router.post('/login', authController.login);
+
+router.post('/register', authController.registerUser);
+
+router.post('/refresh-token', authController.refreshToken);
+
 router.post('/forget-password', authController.forgetPassword);
 
 router.post('/verify-forget-password-code', authController.verifyForgetPassCode);
