905d8fb5e57c8f77b79ce4bf0b5bda0714870c94
MohamadOsama17 <mohamad.abu.ramadan@gmail.com>
2023-09-26 00:14:32 +0300

    - update product schema

--- models/productsModel.js
+++ models/productsModel.js
@@ -11,12 +11,42 @@ const mongoose = require('mongoose');
 }
 
 */
-const productSchema = mongoose.Schema({
+const productSchema = new mongoose.Schema({
   name: {
     required: [true, 'Product name is required !'],
     type: mongoose.Schema.Types.String,
     min: [6, 'Product name length should be more than or equal 6 !'],
     max: [256, 'Product name length should be less than  256 !']
+  },
+  description: {
+    required: [true, 'Product description is required !'],
+    type: mongoose.Schema.Types.String,
+    min: [6, 'Product description length should be more than or equal 6 !'],
+    max: [256, 'Product description length should be less than  256 !']
+  },
+  image: {
+    type: mongoose.Schema.Types.String,
+    required: [true, 'Product image is required !'],
+    validate: {
+      validator: (value) => {
+        return /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i.test(value)
+      },
+      message: 'Invalid product image URL !',
+    }
+  },
+  images: {
+    type: [
+      {
+        type: mongoose.Schema.Types.String,
+        validate: {
+          validator: (value) => {
+            return /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i.test(value)
+          },
+          message: 'Invalid product image URL !',
+        }
+      },
+    ],
+    default: [],
   },
   price: {
     required: [true, 'Product price is required !'],
@@ -30,11 +60,11 @@ const productSchema = mongoose.Schema({
     default: 1,
     min: [1, 'Product quantity should be 1 or greater !'],
   },
-  coverImage: {
-    required: [true, 'Product image is required !'],
-    type: mongoose.Schema.Types.String,
-    cast: 'Invalid image type !',
-  },
+  // coverImage: {
+  //   required: [true, 'Product image is required !'],
+  //   type: mongoose.Schema.Types.String,
+  //   cast: 'Invalid image type !',
+  // },
   images: {
     type: [[''], 'Product images should be array of string !'],
     default: [],
@@ -43,6 +73,17 @@ const productSchema = mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'ProductCategory',
     required: [true, 'Product category id is required !']
+  },
+  rating: {
+    type: mongoose.Schema.Types.Number,
+    cast: 'Invalid rating !',
+    default: 0,
+    min: 0,
+    max: 5,
+  },
+  createdAt: {
+    type: mongoose.Schema.Types.Date,
+    default: Date.now,
   }
 });
 
