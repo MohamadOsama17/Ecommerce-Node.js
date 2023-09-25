12c95751543e52f3d4c46bfdfe95ce1b9519d4f3
MohamadOsama17 <mohamad.abu.ramadan@gmail.com>
2023-09-26 00:14:44 +0300

    - upload product image

--- routes/product/product.js
+++ routes/product/product.js
@@ -4,10 +4,13 @@ const appRoles = require('../../config/roles');
 const productController = require('../../controllers/productController');
 const verifyRole = require('../../middlewares/authroizeRole');
 
+const createUploader = require('../../utils/filesUploader');
+
+const uploader = createUploader('statics/products-images');
 
 router.route('/')
   .get(productController.getAllProdcts)
-  .post(verifyRole([appRoles.Admin, appRoles.Editor]), productController.createProduct);
+  .post(verifyRole([appRoles.Admin, appRoles.Editor]), uploader.single('image'), productController.createProduct);
 
 router.get('/search', productController.search);
 
