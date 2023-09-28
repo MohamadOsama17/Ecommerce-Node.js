63d11c9f18ec79daeea29d084cbccc970fc2bc96
MohamadOsama17 <mohamad.abu.ramadan@gmail.com>
2023-09-28 15:19:55 +0300

    try to save every product image in seperate folder

--- utils/filesUploader.js
+++ utils/filesUploader.js
@@ -1,11 +1,30 @@
 const multer = require('multer');
 
+const fs = require('fs');
+const path = require('path');
+
+const uuid = require('uuid');
+
 const configureStorage = (folderPath) => multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, folderPath);
+  destination: async (req, file, cb) => {
+    // console.log('destination-npm ----');
+    // console.log(req.body);
+    // console.log(uuid.v4());
+    const tempFolderName = Date.now().toString();
+    req.tempFolderName = tempFolderName;
+
+    const newFolderPath = path.join(folderPath,tempFolderName)
+    fs.mkdir(newFolderPath, (err) => {
+      if (err) {
+        console.error(`Error creating folder: ${err}`);
+      } else {
+        console.log(`Folder '${tempFolderName}' created successfully.`);
+      }
+    });
+    cb(null, `${folderPath}/${tempFolderName}`);
   },
   filename: (req, file, cb) => {
-    const fileName = `${Date.now()}-${file.originalname.replace(' ', '-')}`;
+    const fileName = `${Date.now()}-${file.originalname.split(' ').join('-')}`;
     cb(null, fileName);
   }
 });
